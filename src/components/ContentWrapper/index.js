c826c9834f8cc2a969cdb72dcd33f32cddccecee
Christoph Reimers <christoph@superservice-international.com>
2017-12-05 10:38:08 +0100

    move styles to content wrapper

--- src/components/ContentWrapper/index.js
+++ src/components/ContentWrapper/index.js
@@ -1,13 +1,33 @@
+import React from 'react'
 import styled from 'styled-components'
 
 import { maxWidth } from './../../utils/constants'
 
-const ContentWrapper = styled.div`
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+`
+
+const Container = styled.div`
   max-width: ${maxWidth};
   width: 100%;
   margin-left: 15px;
   margin-right: 15px;
   height: 100%;
 `
+
+class ContentWrapper extends React.Component {
+  render() {
+    return(
+      <Wrapper>
+        <Container>
+          {this.props.children}
+        </Container>
+      </Wrapper>
+    )
+  }
+}
 
 export default ContentWrapper
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,10 +9,7 @@ import Title from './Title'
 
 const HeaderOuter = styled.div`
   height: 100vh;
-  width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  width: 100%;
 `
 
 const LogoWrapper = styled.div`
